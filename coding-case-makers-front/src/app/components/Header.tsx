654039dbe1a1e551e6ea54ed66e3a1faf6577dff
solnarux <solnarux@gmail.com>
2024-10-12 15:14:02 -0500

    feat: back init

--- coding-case-makers-front/src/app/components/Header.tsx
+++ coding-case-makers-front/src/app/components/Header.tsx
@@ -1,4 +1,4 @@
-import { ShoppingCart } from 'lucide-react'
+import { LogIn, ShoppingCart } from 'lucide-react'
 import { Button } from '@/app/components/ui/button'
 import React from 'react'
 
@@ -7,10 +7,14 @@ export default function Header() {
     <header className="bg-white shadow-sm">
     <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
       <h1 className="text-2xl font-bold text-gray-900">My Store</h1>
-      <Button variant="outline" size="icon">
+      <div className='flex flex-row gap-2'>
+        <Button variant="outline" size="icon">
         <ShoppingCart className="h-5 w-5" />
-        <span className="sr-only">View cart</span>
       </Button>
+      <Button variant="outline" size="icon">
+        <LogIn className='h-5 w-5'/>
+      </Button>
+      </div>
     </div>
   </header>
   )
